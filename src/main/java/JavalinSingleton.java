11e38022830e509fc2b1c4ae62eb31b874b6baa6
KESAVANMANIARASU <kesavanm0904@gmail.com>
2024-07-22 17:02:09 +0530

    commit

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -1,27 +1,22 @@
-
-
 import io.javalin.Javalin;
 
 public class JavalinSingleton {
     /**
-     * Assignment: return the status code for a generic "client side error" in the http response
+     * Assignment: return the status code for a generic "client side error" in the HTTP response
      *
      * You will not need to run app.start in this method. The test cases, or main method, will do this for you - this
      * method only needs to return a properly configured Javalin Server, represented by the 'app' object created below.
      *
      * Please refer to the "StatusCodes.MD" file.
      */
-    public static Javalin getInstance(){
+    public static Javalin getInstance() {
         Javalin app = Javalin.create();
-        
 
         app.get("/client-side-err", ctx -> {
-        
-            //write logic here
-            
+            // Setting the status code to 400 to represent a client-side error
+            ctx.status(400);
         });
 
         return app;
     }
-    
 }
